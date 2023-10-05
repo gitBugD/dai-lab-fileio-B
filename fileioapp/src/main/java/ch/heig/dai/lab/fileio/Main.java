a837ed4ab224202b26f9e77ea92c4e44ce5e0111
karilla <bdelay155@gmail.com>
2023-10-05 13:33:27 +0200

    Create folders for karilla

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
@@ -3,11 +3,11 @@ package ch.heig.dai.lab.fileio;
 import java.io.File;
 
 // *** TODO: Change this to import your own package ***
-import ch.heig.dai.lab.fileio.jehrensb.*;
+import ch.heig.dai.lab.fileio.karilla.*;
 
 public class Main {
     // *** TODO: Change this to your own name ***
-    private static final String newName = "Jean-Claude Van Damme";
+    private static final String newName = "Karilla";
 
     /**
      * Main method to transform files in a folder.
