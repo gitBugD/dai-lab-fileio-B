344f278c9c345a176c29ebbbc8e27590812585ba
acm <a-mango@users.noreply.github.com>
2023-10-05 14:36:30 +0200

    Implement EncodingSelector for a-mango

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/amango/EncodingSelector.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/amango/EncodingSelector.java
@@ -2,6 +2,7 @@ package ch.heig.dai.lab.fileio.amango;
 
 import java.io.File;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class EncodingSelector {
 
@@ -17,7 +18,18 @@ public class EncodingSelector {
      * @return the encoding of the file, or null if the extension is not recognized
      */
     public Charset getEncoding(File file) {
-        // TODO: implement the method body here
-        return null;
+        String fileName = file.getName();
+        int extIndex = fileName.lastIndexOf(".");
+
+        if (extIndex < 0)
+            return null;
+
+        return switch (fileName.substring(extIndex)) {
+            case ".utf8" -> StandardCharsets.UTF_8;
+            case ".txt" -> StandardCharsets.US_ASCII;
+            case ".utf16be" -> StandardCharsets.UTF_16BE;
+            case ".utf16le" -> StandardCharsets.UTF_16LE;
+            default -> null;
+        };
     }
 }
