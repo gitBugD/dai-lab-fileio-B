2d6f0d7e8c39489d9e8011768e05d5efe1fe6fc1
Alessio Giuliano <giuliano.alessio36@gmail.com>
2023-10-05 14:44:51 +0200

    Implemented EncodingSelector

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/AlessioGiuliano/EncodingSelector.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/AlessioGiuliano/EncodingSelector.java
@@ -2,6 +2,7 @@ package ch.heig.dai.lab.fileio.AlessioGiuliano;
 
 import java.io.File;
 import java.nio.charset.Charset;
+import java.util.Map;
 
 public class EncodingSelector {
 
@@ -17,7 +18,21 @@ public class EncodingSelector {
      * @return the encoding of the file, or null if the extension is not recognized
      */
     public Charset getEncoding(File file) {
-        // TODO: implement the method body here
+        String fileName = file.getName();
+        int index = fileName.lastIndexOf(".");
+        if (index > 0)
+        {
+            String extension = fileName.substring(index);
+            if (EXTENSION_TO_CHARSET.containsKey(extension))
+            {
+                return Charset.forName(EXTENSION_TO_CHARSET.get(extension));
+            }
+        }
         return null;
     }
+    private final static Map<String, String> EXTENSION_TO_CHARSET = Map.of(
+        ".utf8", "UTF-8",
+        ".txt", "US-ASCII", 
+        ".utf16be", "UTF-16BE",
+        ".utf16le", "UTF-16LE");
 }
