9754046489b42eadbb4eba0f87655fea29916298
Balkghar <hugo.germano8@gmail.com>
2023-10-08 11:24:16 +0200

    implementation of read and write file

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/balkghar/FileReaderWriter.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/balkghar/FileReaderWriter.java
@@ -2,6 +2,7 @@ package ch.heig.dai.lab.fileio.balkghar;
 
 import java.io.*;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class FileReaderWriter {
 
@@ -12,10 +13,18 @@ public class FileReaderWriter {
      * @return the content of the file as a String, or null if an error occurred.
      */
     public String readFile(File file, Charset encoding) {
-        // TODO: Implement the method body here. 
         // Use the ...Stream and ...Reader classes from the java.io package.
         // Make sure to close the streams and readers at the end.
-        return null;
+        String stringToReturn = "";
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file.getAbsolutePath()), encoding))) {
+            while(reader.ready()) {
+                stringToReturn += reader.readLine();
+            }
+            reader.close();
+        }catch (Exception e) {
+            return null;
+        }
+        return stringToReturn;
     }
 
     /**
@@ -26,9 +35,16 @@ public class FileReaderWriter {
      * @return true if the file was written successfully, false otherwise
      */
     public boolean writeFile(File file, String content, Charset encoding) {
-        // TODO: Implement the method body here. 
         // Use the ...Stream and ...Reader classes from the java.io package.
         // Make sure to flush the data and close the streams and readers at the end.
-        return false;
+
+        try(BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file.getAbsolutePath()),encoding))){
+            writer.write(content);
+            writer.flush();
+            writer.close();
+            return true;
+        }catch (Exception e) {
+            return false;
+        }
     }
 }
