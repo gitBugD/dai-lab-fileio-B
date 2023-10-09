90dc79dfec9dd7e92ec70ceb689960d1c6768410
Balkghar <hugo.germano8@gmail.com>
2023-10-09 17:56:59 +0200

    implementatio of the feature transformer

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/balkghar/Transformer.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/balkghar/Transformer.java
@@ -49,8 +49,23 @@ public class Transformer {
      * @return the transformed string
      */
     public String wrapAndNumberLines(String source) {
+
         // Use the StringBuilder class to build the result string.
+        StringBuilder strTemp =  new StringBuilder();
+
+        String words[]=source.split("\\s");
+
+        for(int i = 0; i < words.length; ++i){
+            if(i % numWordsPerLine == 0){
+                if(i / numWordsPerLine > 0)
+                    strTemp.append("\n");
+                strTemp.append((i / numWordsPerLine + 1) + ".");
+            }
+            strTemp.append(" " + words[i]);
+        }
         
-        return "";
+        strTemp.append("\n");
+
+        return strTemp.toString();
     }
 }   
