630af0cd6f57c24a204b28f968c89a54b6012932
“emilie-h” <emilie.bressoud@heig-vd.ch>
2023-10-09 15:38:56 +0200

    add reader and writer

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/emilieh/FileReaderWriter.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/emilieh/FileReaderWriter.java
@@ -1,6 +1,6 @@
 package ch.heig.dai.lab.fileio.emilieh;
 
-import java.io.File;
+import java.io.*;
 import java.nio.charset.Charset;
 
 public class FileReaderWriter {
@@ -11,11 +11,18 @@ public class FileReaderWriter {
      * @param encoding
      * @return the content of the file as a String, or null if an error occurred.
      */
-    public String readFile(File file, Charset encoding) {
-        // TODO: Implement the method body here. 
+    public String readFile(File file, Charset encoding) throws IOException {
         // Use the ...Stream and ...Reader classes from the java.io package.
         // Make sure to close the streams and readers at the end.
-        return null;
+        var reader = new FileReader(file, encoding);
+        StringBuilder s = new StringBuilder();
+        int c;
+        while ((c = reader.read()) != -1) {
+           s.append((char) c);
+        }
+        reader.close();
+
+        return s.toString();
     }
 
     /**
@@ -25,10 +32,20 @@ public class FileReaderWriter {
      * @param encoding the encoding to use
      * @return true if the file was written successfully, false otherwise
      */
-    public boolean writeFile(File file, String content, Charset encoding) {
+    public boolean writeFile(File file, String content, Charset encoding) throws IOException {
         // TODO: Implement the method body here. 
         // Use the ...Stream and ...Reader classes from the java.io package.
         // Make sure to flush the data and close the streams and readers at the end.
+        var reader = new BufferedReader(new StringReader(content));
+        var writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), encoding));
+        int c;
+        while ((c = reader.read()) != -1) {
+            writer.write(c);
+        }
+        writer.flush();
+        writer.close();
+        reader.close();
+
         return false;
     }
 }
