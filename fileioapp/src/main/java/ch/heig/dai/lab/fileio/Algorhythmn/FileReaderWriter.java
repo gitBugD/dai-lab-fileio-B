394e29bb91dd976ceee255ecc088efa9a4453eda
Glodi <Glodi_Domingos@hotmail.com>
2023-10-12 14:50:24 +0200

    FileReader v1

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Algorhythmn/FileReaderWriter.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Algorhythmn/FileReaderWriter.java
@@ -12,10 +12,16 @@ public class FileReaderWriter {
      * @return the content of the file as a String, or null if an error occurred.
      */
     public String readFile(File file, Charset encoding) {
-        // TODO: Implement the method body here. 
-        // Use the ...Stream and ...Reader classes from the java.io package.
-        // Make sure to close the streams and readers at the end.
-        return null;
+        StringBuilder output = new StringBuilder();
+        try (var reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), encoding))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Exception: " + e);
+        }
+        return output.toString();
     }
 
     /**
@@ -26,9 +32,13 @@ public class FileReaderWriter {
      * @return true if the file was written successfully, false otherwise
      */
     public boolean writeFile(File file, String content, Charset encoding) {
-        // TODO: Implement the method body here. 
-        // Use the ...Stream and ...Reader classes from the java.io package.
-        // Make sure to flush the data and close the streams and readers at the end.
-        return false;
+        try (var writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), encoding))) {
+            writer.write(content + '\r');
+            writer.flush();
+        } catch (IOException e) {
+            System.out.println("Exception: " + e);
+            return false;
+        }
+        return true;
     }
 }
