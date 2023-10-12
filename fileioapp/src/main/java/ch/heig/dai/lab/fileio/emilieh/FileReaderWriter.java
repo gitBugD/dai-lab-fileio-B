d4ffa8ffb0f5f719daaf6c49009bd1dbe9297b93
“emilie-h” <emilie.bressoud@heig-vd.ch>
2023-10-12 12:01:14 +0200

    with try catch

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/emilieh/FileReaderWriter.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/emilieh/FileReaderWriter.java
@@ -7,45 +7,52 @@ public class FileReaderWriter {
 
     /**
      * Read the content of a file with a given encoding.
-     * @param file the file to read. 
+     *
+     * @param file     the file to read.
      * @param encoding
      * @return the content of the file as a String, or null if an error occurred.
      */
-    public String readFile(File file, Charset encoding) throws IOException {
+    public String readFile(File file, Charset encoding) {
         // Use the ...Stream and ...Reader classes from the java.io package.
         // Make sure to close the streams and readers at the end.
-        var reader = new FileReader(file, encoding);
         StringBuilder s = new StringBuilder();
-        int c;
-        while ((c = reader.read()) != -1) {
-           s.append((char) c);
+        try (var stream = new FileInputStream(file);
+             var reader = new InputStreamReader(stream, encoding)) {
+            int c;
+            while ((c = reader.read()) != -1) {
+                s.append((char) c);
+            }
+        } catch (IOException e) {
+            System.out.println("Error while reading file");
         }
-        reader.close();
-
         return s.toString();
     }
 
     /**
-     * Write the content to a file with a given encoding. 
-     * @param file the file to write to
-     * @param content the content to write
+     * Write the content to a file with a given encoding.
+     *
+     * @param file     the file to write to
+     * @param content  the content to write
      * @param encoding the encoding to use
      * @return true if the file was written successfully, false otherwise
      */
-    public boolean writeFile(File file, String content, Charset encoding) throws IOException {
+    public boolean writeFile(File file, String content, Charset encoding) {
         // TODO: Implement the method body here. 
         // Use the ...Stream and ...Reader classes from the java.io package.
         // Make sure to flush the data and close the streams and readers at the end.
-        var reader = new BufferedReader(new StringReader(content));
-        var writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), encoding));
-        int c;
-        while ((c = reader.read()) != -1) {
-            writer.write(c);
-        }
-        writer.flush();
-        writer.close();
-        reader.close();
+        try (
+                var stream = new FileOutputStream(file);
+                var writer = new OutputStreamWriter(stream, encoding)
+        ) {
+            writer.write(content);
+            writer.flush();
+            writer.close();
+            stream.close();
+            return true;
+        } catch (IOException e) {
+            System.out.println("Error while writing file");
+            return false;
 
-        return false;
+        }
     }
 }
