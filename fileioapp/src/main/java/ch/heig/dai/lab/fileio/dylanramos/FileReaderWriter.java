6794cb98daf5a7af35455a6266ebcc57739a72af
dylanramos <dylan.oliveiraramos@heig-vd.ch>
2023-10-12 13:31:34 +0200

    Implement FileReaderWriter for dylanramos

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/dylanramos/FileReaderWriter.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/dylanramos/FileReaderWriter.java
@@ -12,10 +12,20 @@ public class FileReaderWriter {
      * @return the content of the file as a String, or null if an error occurred.
      */
     public String readFile(File file, Charset encoding) {
-        // TODO: Implement the method body here. 
-        // Use the ...Stream and ...Reader classes from the java.io package.
-        // Make sure to close the streams and readers at the end.
-        return null;
+        try(var is = new BufferedReader(new InputStreamReader(new FileInputStream(file.getName()), encoding))){
+            var content = new StringBuilder();
+            String line;
+
+            while((line = is.readLine()) != null){
+                content.append(line);
+            }
+
+            is.close();
+
+            return content.toString();
+        } catch (IOException e) {
+            return null;
+        }
     }
 
     /**
@@ -26,9 +36,13 @@ public class FileReaderWriter {
      * @return true if the file was written successfully, false otherwise
      */
     public boolean writeFile(File file, String content, Charset encoding) {
-        // TODO: Implement the method body here. 
-        // Use the ...Stream and ...Reader classes from the java.io package.
-        // Make sure to flush the data and close the streams and readers at the end.
-        return false;
+        try(var os = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), encoding))){
+            os.write(content);
+            os.close();
+
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
     }
 }
