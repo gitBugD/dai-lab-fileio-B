e6d819e26f1b46e3f1a8f3939fb9a253c60c1204
Istomine <swagman@swag.com>
2023-10-12 14:40:15 +0200

    Finised FileReaderWriter

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Istomine/FileReaderWriter.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Istomine/FileReaderWriter.java
@@ -11,23 +11,44 @@ public class FileReaderWriter {
      * @return the content of the file as a String, or null if an error occurred.
      */
     public String readFile(File file, Charset encoding) {
-        // TODO: Implement the method body here. 
-        // Use the ...Stream and ...Reader classes from the java.io package.
-        // Make sure to close the streams and readers at the end.
-        return null;
+
+        StringBuilder output = new StringBuilder();
+
+        try( var read = new BufferedReader(new InputStreamReader( new FileInputStream(file),encoding ))) {
+
+            String line;
+            while((line = read.readLine()) != null){
+                output.append(line);
+            }
+            read.close();
+
+            return output.toString();
+
+        } catch (IOException e) {
+            System.out.println("Exception : " + e);
+            return null;
+        }
     }
 
     /**
-     * Write the content to a file with a given encoding. 
+     * Write the content to a file with a given encoding.
      * @param file the file to write to
      * @param content the content to write
      * @param encoding the encoding to use
      * @return true if the file was written successfully, false otherwise
      */
     public boolean writeFile(File file, String content, Charset encoding) {
-        // TODO: Implement the method body here. 
-        // Use the ...Stream and ...Reader classes from the java.io package.
-        // Make sure to flush the data and close the streams and readers at the end.
-        return false;
+        try( var writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), encoding))) {
+
+            writer.write(content);
+            writer.flush();
+            writer.close();
+            return true;
+
+        }catch (IOException e){
+            System.out.println("Exception : " + e);
+            return false;
+        }
+
     }
 }
