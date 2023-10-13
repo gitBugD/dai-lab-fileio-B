14b794d6807253d833e963fe3bced09074268df5
Istomine <swagman@swag.com>
2023-10-13 15:33:54 +0200

    EncodingSelector finised

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Istomine/EncodingSelector.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Istomine/EncodingSelector.java
@@ -2,6 +2,8 @@ package ch.heig.dai.lab.fileio.Istomine;
 
 import java.io.File;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 public class EncodingSelector {
 
@@ -12,12 +14,25 @@ public class EncodingSelector {
      *   - .txt:     US-ASCII
      *   - .utf16be: UTF-16BE
      *   - .utf16le: UTF-16LE
-     * 
+     *
      * @param file the file to get the encoding from
      * @return the encoding of the file, or null if the extension is not recognized
      */
-    public Charset getEncoding(File file) {
-        // TODO: implement the method body here
-        return null;
+    private Map<String,Charset> charMap = Map.of(
+            "utf8",StandardCharsets.UTF_8,
+            "txt",StandardCharsets.US_ASCII,
+            "utf16be",StandardCharsets.UTF_16BE,
+            "utf16le",StandardCharsets.UTF_16LE
+    );
+    public Charset getEncoding(File file){
+
+        String name = file.getName();
+        int lastIndexOf = name.lastIndexOf(".");
+
+        if (lastIndexOf <= 0) {
+            return null;
+        }
+
+        return charMap.get(name.substring(lastIndexOf + 1));
     }
 }
