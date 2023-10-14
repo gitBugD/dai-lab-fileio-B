a5d7d66902591b3848a01f24376b782521e7dafb
gitBugD <diana.laurenti@heig-vd.ch>
2023-10-14 21:01:41 +0200

    implement transformer

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/gitBugD/Transformer.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/gitBugD/Transformer.java
@@ -5,9 +5,14 @@ public class Transformer {
     private final String newName;
     private final int numWordsPerLine;
 
+    public static void main(String[] args) {
+        Transformer t = new Transformer("ciao", 2);
+        t.wrapAndNumberLines("Example of a very long sentence blabla.");
+    }
+
     /**
      * Constructor
-     * Initialize the Transformer with the name to replace "Chuck Norris" with 
+     * Initialize the Transformer with the name to replace "Chuck Norris" with
      * and the number of words per line to use when wrapping the text.
      * @param newName the name to replace "Chuck Norris" with
      * @param numWordsPerLine the number of words per line to use when wrapping the text
@@ -23,8 +28,9 @@ public class Transformer {
      * @return the transformed string
      */
     public String replaceChuck(String source) {
-        // TODO: Implement the method body here.
-        return "";
+        final String toReplace = "Chuck Norris";
+
+        return source.replaceAll(toReplace, this.newName);
     }
 
     /**
@@ -33,8 +39,20 @@ public class Transformer {
      * @return the transformed string
      */
     public String capitalizeWords(String source) {
-        // TODO: Implement the method body here.
-        return "";
+        final String delimiter = " ";
+        String[] splitSource = source.split(delimiter);
+        int lengthSentence = splitSource.length;
+        String[] splitCapitalized = new String[lengthSentence];
+
+        for(int i = 0; i < lengthSentence; ++i){
+            String word = splitSource[i];
+            StringBuilder result = new StringBuilder();
+            result.append(Character.toUpperCase(word.charAt(0)));
+            result.append(word.substring(1));
+            splitCapitalized[i] = String.valueOf(result);
+        }
+
+        return String.join(delimiter, splitCapitalized);
     }
 
     /**
@@ -44,8 +62,31 @@ public class Transformer {
      * @return the transformed string
      */
     public String wrapAndNumberLines(String source) {
-        // TODO: Implement the method body here.
-        // Use the StringBuilder class to build the result string.
-        return "";
+        StringBuilder result = new StringBuilder();
+        int lengthSentence = source.length();
+        int lineNumber = 1;
+        int numWords = 0;
+        int lineStart = 0;
+
+        for(int lineEnd = 0; lineEnd < lengthSentence; ++lineEnd){
+            if(lineEnd + 1 == lengthSentence){
+                result.append(lineNumber).append(". ");
+                result.append(source, lineStart, lineEnd + 1);
+                result.append("\n");
+            }
+            else if(source.charAt(lineEnd) == ' '){
+                ++numWords;
+                if(numWords == numWordsPerLine){
+                    result.append(lineNumber).append(". ");
+                    result.append(source, lineStart, lineEnd);
+                    result.append("\n");
+
+                    ++lineNumber;
+                    numWords = 0;
+                    lineStart = lineEnd + 1;
+                }
+            }
+        }
+        return String.valueOf(result);
     }
 }   
