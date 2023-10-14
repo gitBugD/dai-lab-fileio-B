318f298923f05458c48c192e9c02f89247108282
Maxime-sch <55839375+Maxime-sch@users.noreply.github.com>
2023-10-14 21:29:47 +0200

    Implement encodingSelector

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/maximesch/EncodingSelector.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/maximesch/EncodingSelector.java
@@ -2,6 +2,8 @@ package ch.heig.dai.lab.fileio.maximesch;
 
 import java.io.File;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 public class EncodingSelector {
 
@@ -16,8 +18,30 @@ public class EncodingSelector {
      * @param file the file to get the encoding from
      * @return the encoding of the file, or null if the extension is not recognized
      */
+
+    private static final Map<String, Charset> dotExtToExt = Map.of(
+            ".utf8" , StandardCharsets.UTF_8,
+            ".txt", StandardCharsets.US_ASCII,
+            ".utf16be", StandardCharsets.UTF_16BE,
+            ".utf16le", StandardCharsets.UTF_16LE
+    );
+
     public Charset getEncoding(File file) {
-        // TODO: implement the method body here
+
+        String fileName = file.getName();
+        int lastDotBeforeExt = fileName.lastIndexOf('.');
+
+        if(lastDotBeforeExt == -1){ // no '.' found
+            return null;
+        }
+
+        String extensionStr = fileName.substring(lastDotBeforeExt);
+        if(dotExtToExt.containsKey(extensionStr)){
+            return dotExtToExt.get(extensionStr);
+        }
+
+        //if we still have not returned anything at that point, there is a "."
+        // but no valid extension after and we return null
         return null;
     }
 }
