2f60a6e833d88a012248c217c9c971c507851d3e
Maxime-sch <55839375+Maxime-sch@users.noreply.github.com>
2023-10-14 21:58:49 +0200

    Implement fileReaderWriter

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/maximesch/FileReaderWriter.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/maximesch/FileReaderWriter.java
@@ -12,10 +12,21 @@ public class FileReaderWriter {
      * @return the content of the file as a String, or null if an error occurred.
      */
     public String readFile(File file, Charset encoding) {
-        // TODO: Implement the method body here. 
-        // Use the ...Stream and ...Reader classes from the java.io package.
-        // Make sure to close the streams and readers at the end.
-        return null;
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(file, encoding))){
+            StringBuilder result = new StringBuilder();
+            String curLine;
+
+            while((curLine = reader.readLine()) != null){
+                result.append(curLine);
+                result.append('\n');
+            }
+            reader.close();
+            return result.toString();
+        }
+        catch (IOException e){
+            return null;
+        }
     }
 
     /**
@@ -26,9 +37,15 @@ public class FileReaderWriter {
      * @return true if the file was written successfully, false otherwise
      */
     public boolean writeFile(File file, String content, Charset encoding) {
-        // TODO: Implement the method body here. 
-        // Use the ...Stream and ...Reader classes from the java.io package.
-        // Make sure to flush the data and close the streams and readers at the end.
-        return false;
+
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), encoding))){
+            writer.write(content);
+            writer.flush();
+            writer.close();
+            return true;
+        }
+        catch (IOException e){
+            return false;
+        }
     }
 }
