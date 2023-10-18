63759c3d83bde54da89800c5d8550c675b9b3ef4
gitBugD <diana.laurenti@heig-vd.ch>
2023-10-18 16:54:12 +0200

    Fix fileReaderWriter

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/gitBugD/FileReaderWriter.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/gitBugD/FileReaderWriter.java
@@ -12,23 +12,48 @@ public class FileReaderWriter {
      * @return the content of the file as a String, or null if an error occurred.
      */
     public String readFile(File file, Charset encoding) {
-        // TODO: Implement the method body here. 
-        // Use the ...Stream and ...Reader classes from the java.io package.
-        // Make sure to close the streams and readers at the end.
+        try{
+            var reader = new BufferedReader(
+                    new InputStreamReader(new FileInputStream(file), encoding));
+
+            StringBuilder contentBuilder = new StringBuilder();
+            String currentLine;
+
+            while ((currentLine = reader.readLine()) != null)
+            {
+                contentBuilder.append(currentLine).append("\n");
+            }
+
+            reader.close();
+            return contentBuilder.toString();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
         return null;
     }
 
     /**
-     * Write the content to a file with a given encoding. 
+     * Write the content to a file with a given encoding.
      * @param file the file to write to
      * @param content the content to write
      * @param encoding the encoding to use
      * @return true if the file was written successfully, false otherwise
      */
     public boolean writeFile(File file, String content, Charset encoding) {
-        // TODO: Implement the method body here. 
-        // Use the ...Stream and ...Reader classes from the java.io package.
-        // Make sure to flush the data and close the streams and readers at the end.
+        try{
+            file.createNewFile();
+            var writer = new BufferedWriter(
+                    new OutputStreamWriter(new FileOutputStream(file), encoding));
+
+            writer.write(content);
+            writer.write("\r\n");
+
+            writer.flush();
+            writer.close();
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
         return false;
     }
 }
